371ff2168e22f893f5b724b512a6b212512cb1c7
Marcin Badurowicz <m@badurowicz.net>
2025-01-26 12:36:37 +0100

    add instruction screen

--- include/instruction.hpp
+++ include/instruction.hpp
@@ -1,8 +1,13 @@
 #include "level.hpp"
 
-class Outro2Screen : public Level
+class InstructionScreen : public Level
 {
 public:
+    InstructionScreen(String text)
+    {
+        this->text = text;
+    }
+
     virtual void setup(std::shared_ptr<LGFX_Sprite> buffer) override
     {
         this->buffer = buffer;
@@ -13,31 +18,27 @@ public:
     {
         buffer->clear();
         buffer->setTextDatum(middle_center);
-        buffer->setCursor(0, 12);
-        buffer->setFont(&fonts::efontJA_12);
+        buffer->setCursor(0, 50);
+        buffer->setTextColor(WHITE);
+        buffer->setFont(&fonts::efontJA_24_b);
 
         for (int i = 0; i < step; i++)
         {
-            buffer->print(texts[text][i]);
+            buffer->print(text[i]);
         }
 
         if (millis() - last > 50)
         {
-            if (step < texts[text].length())
+            if (step < text.length())
                 step++;
 
             last = millis();
         }
 
-        if (step == texts[text].length() && button)
+        if (step == text.length() && button)
         {
-            text++;
-            step = 0;
 
-            if (text == 2)
-            {
-                return LevelResult::Finished;
-            }
+            return LevelResult::Finished;
         }
 
         return LevelResult::Continue;
@@ -45,8 +46,7 @@ public:
 
 private:
     long start = 0;
-    String texts[2] = {"I'm in!", "Hacking..."};
-    int text = 0;
+    String text;
     int step = 0;
     long last = 0;
 };
